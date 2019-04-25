0a893d47a815deec76f07548cba8c52e41526beb
marco2012 <marco.fox.marco@gmail.com>
2019-04-25 23:05:16 +0200

    Initial Commit

---
+++ CurrencyConvert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConvert
+//
+//  Created by Marco on 25/04/2019.
+//  Copyright © 2019 vikings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
